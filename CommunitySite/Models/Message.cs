a4474f61770ec0b7513a4ff9c0b4fb551a2fca35
betettag <49417436+betettag@users.noreply.github.com>
2020-01-15 00:02:55 -0800

    updated validation it finally works

--- CommunitySite/Models/Message.cs
+++ CommunitySite/Models/Message.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,21 +14,16 @@ namespace CommunitySite.Models
             PubDate = DateTime.Now;
         }
         public int MessageID { get; set; }
-        {
-            PubDate = DateTime.Now;
-        }
-        public int MessageID { get; set; }
-        [StringLength(100, MinimumLength = 2)]
+        public User Author { get; set; }
         [Required(ErrorMessage = "Please enter your UserName/Name")]
         public string User { get; set; }
-        [DataType(DataType.Text)]
-        [Range(5, 230, ErrorMessage = "Please something meaningful")]
         [Required(ErrorMessage = "No message. if you dont put one then whats the point?")]
         public string Body { get; set; }
         [Required(ErrorMessage = "WE NEED TO KNOW WHERE YOU LIVE")]
         public string Address { get; set; }
         [Required(ErrorMessage = "Enter a phone please. I pinky swear we wont call")]
         public string Phone { get; set; }
+        [EmailAddress]
         [Required(ErrorMessage = "Please enter your email address")]
         [RegularExpression(".+\\@.+\\..+",
             ErrorMessage = "Please enter a valid email address")]
